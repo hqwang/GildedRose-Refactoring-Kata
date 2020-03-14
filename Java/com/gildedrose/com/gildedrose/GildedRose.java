0eedfed38d11a7552fd21f25bfc2565f979e5cce
fighterwang <fighterwang@tencent.com>
2020-03-14 15:03:21 +0800

    添加商品常量.

---
+++ Java/com/gildedrose/com/gildedrose/GildedRose.java
@@ -0,0 +1,62 @@
+package com.gildedrose;
+
+class GildedRose {
+    Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (int i = 0; i < items.length; i++) {
+            if (!items[i].name.equals(NameList.AGED_BRIE)
+                    && !items[i].name.equals(NameList.TAFKAL80ETC_CONCERT)) {
+                if (items[i].quality > 0) {
+                    if (!items[i].name.equals(NameList.HAND_OF_RAGNAROS)) {
+                        items[i].quality = items[i].quality - 1;
+                    }
+                }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+
+                    if (items[i].name.equals(NameList.TAFKAL80ETC_CONCERT)) {
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!items[i].name.equals(NameList.HAND_OF_RAGNAROS)) {
+                items[i].sellIn = items[i].sellIn - 1;
+            }
+
+            if (items[i].sellIn < 0) {
+                if (!items[i].name.equals(NameList.AGED_BRIE)) {
+                    if (!items[i].name.equals(NameList.TAFKAL80ETC_CONCERT)) {
+                        if (items[i].quality > 0) {
+                            if (!items[i].name.equals(NameList.HAND_OF_RAGNAROS)) {
+                                items[i].quality = items[i].quality - 1;
+                            }
+                        }
+                    } else {
+                        items[i].quality = items[i].quality - items[i].quality;
+                    }
+                } else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Java/com/gildedrose/com/gildedrose/NameList.java
@@ -0,0 +1,12 @@
+package com.gildedrose;
+
+public class NameList {
+
+    public static final String DEXTERITY_VEST = "+5 Dexterity Vest";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String ELIXIR_OF_MONGOOSE = "Elixir of the Mongoose";
+    public static final String HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    public static final String TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String MANA_CAKE = "Conjured Mana Cake";
+
+}
